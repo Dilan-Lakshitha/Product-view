b57307c0d35f44c6beda870ce9eb30787ce28f46
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-10 13:56:38 +0530

    filtered

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,14 @@ export class ProductListComponent implements OnInit{
     imageWidth: number=50;
     imageMargin: number=2;
     showImage: boolean=false;
-    listFilter:String ="cart";
+    private _listFilter: string='';
+    get listFilter():string{
+        return this._listFilter;
+    }
+    set listFilter(value:string){
+        this._listFilter=value;
+        console.log('In setter:',value);
+    }
     products : Iproduct[] =[
         {
             "productId": 1,
