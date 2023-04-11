a3e8ec1520af82d78040f8c35d50dc8798626095
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-11 20:47:49 +0530

    passing parameter to route

--- src/app/products/products-detail.component.ts
+++ src/app/products/products-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products-detail',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ProductsDetailComponent {
   PageTitle: string ='Product Details'
+  constructor(private route:ActivatedRoute){}
+
+
+    ngOnInit(): void {
+      const id=Number(this.route.snapshot.paramMap.get('id'));
+      this.PageTitle +=`:${id}`;
+    }
 }
