d356cfb9509e697da65154a83e32b74b597e637a
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-11 10:12:07 +0530

    Subscribing to an Observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { StartComponent } from './shared/star.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   
   ],
   providers: [],
